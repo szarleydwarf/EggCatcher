c4408bd19feae188e0dd156e79cf4e100e064fe1
Szarley <szarleydwarf@gmail.com>
2017-08-24 15:03:27 +0100

    First Fetch
    Intro Scene
    Menu
     - start working
    - settings todo
    -credits todo
    - quit working ??
    gameplay scene added

---
+++ Egg Catcher/Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void Quit(){
+		Application.Quit ();
+	}
+
+	public void ShowCredits() {
+		
+	}
+
+	public void RunGame(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void Settings(){
+		
+	}
+}
---
+++ Egg Catcher/Assets/Scripts/PlayIntro.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class PlayIntro : MonoBehaviour {
+	public RawImage image;
+	public VideoClip vClip;
+
+	private VideoPlayer vPlayer;
+
+	private VideoSource vSource;
+	private AudioSource aSource;
+
+	// Use this for initialization
+	void Start () {
+//		((MovieTexture)GetComponent <Renderer> ().material.mainTexture).Play ();
+		Application.runInBackground = true;
+		StartCoroutine (playVideo());
+	}
+
+	IEnumerator playVideo() {
+		vPlayer = gameObject.AddComponent<VideoPlayer> ();
+		aSource = gameObject.AddComponent<AudioSource> ();
+
+		vPlayer.playOnAwake = false;
+		aSource.playOnAwake = false;
+		aSource.Pause ();
+
+		vPlayer.source = VideoSource.VideoClip;
+
+		vPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+		vPlayer.EnableAudioTrack (0, true);
+		vPlayer.SetTargetAudioSource (0, aSource);
+
+		vPlayer.clip = vClip;
+		vPlayer.Prepare ();
+
+		WaitForSeconds waitTime = new WaitForSeconds (1);
+		while(!vPlayer.isPrepared){
+//			Debug.Log ("Preparing");
+			yield return waitTime;
+			break;
+		}
+
+//		Debug.Log ("Vid ready");
+		image.texture = vPlayer.texture;
+
+		vPlayer.Play ();
+		aSource.Play ();
+		float duration = (float)vClip.length;
+		WaitForSeconds waitTimePlaying = new WaitForSeconds (1);
+		while(vPlayer.isPlaying){
+			yield return waitTimePlaying;
+		}
+
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
