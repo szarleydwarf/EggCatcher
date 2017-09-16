714b20c6c0914362c33fc628d858e757fd1a3fb9
Szarley <szarleydwarf@gmail.com>
2017-09-16 15:39:05 +0100

    16/09/17
    finally fixed of dispkaying lifes on the screen
    removing if egg broke :)

--- Egg Catcher/Assets/Scripts/DestroyOnContact.cs
+++ Egg Catcher/Assets/Scripts/DestroyOnContact.cs
@@ -8,20 +8,36 @@ public class DestroyOnContact : MonoBehaviour {
 	public GameObject brokenEgg;
 	public Text points;
 
-	private int score = 0, lifes = 3;
+	private int score = 0, lifes = 3, maxLifes= 0, lifeUp = 1;
+	double growthModifier = 0.5;
+	void Start(){
+		maxLifes = GameObject.Find ("GameController").GetComponent<GameController> ().getMaxLifes ();
+//		lifeUp = computeLifesUp (score, 1.5);
+	}
 
 	void OnTriggerEnter2D(Collider2D other){
+		lifes = GameObject.Find ("GameController").GetComponent<GameController> ().getCurrentLifes ();
 		if(this.name.CompareTo ("Basket_B")==0){
 //			Debug.Log ("ADD point" );
 			score++;
 			points.text = "SCORE: " + score;
+
+			Debug.Log ("lifeUp " + lifeUp + " / " + score);
+			if(score % lifeUp == 0 && lifes < maxLifes){
+				lifes++;
+				lifeUp = computeLifesUp (score, growthModifier);
+			}
+
 		}else {
 			lifes--;
 			Instantiate (brokenEgg, new Vector2 (other.transform.position.x, other.transform.position.y),Quaternion.identity);
-			GameObject.Find ("GameManager").GetComponent<GameManager> ().setLifes (lifes);
 		}
+		GameObject.Find ("GameController").GetComponent<GameController> ().setCurrentLifes (lifes);
+		GameObject.Find ("GameController").GetComponent<GameController> ().updateLifes ();
 		Destroy (other.gameObject);
 	}
 
-
+	int computeLifesUp(int score, double growthModifier){
+		return (int)((score * 10) * (score * growthModifier));
+	}
 }
---
+++ Egg Catcher/Assets/Scripts/GameController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public GameObject[] eggs, lifesEggs;
+	public GameObject[] spPoints;
+
+	private int maxLifes = 10, currentLifes = 3;
+	private float instantiateTimer = 2f;
+
+	// Use this for initialization
+	void Awake () {
+		lifesEggs = new GameObject[maxLifes];
+		instantiateLifes ();
+	}
+
+	public void setCurrentLifes(int lifeNum){
+		currentLifes = lifeNum;
+	}
+
+	public int getMaxLifes () {
+		return maxLifes;
+	}
+	public int getCurrentLifes () {
+		return currentLifes;
+	}
+
+	void Update() {
+		instantiateTimer -= Time.deltaTime;
+		if(instantiateTimer <= 0) {
+			int randomNumber = Random.Range (0, eggs.Length);
+			int randomSP = Random.Range (0, spPoints.Length);
+
+			Vector2 spPos = spPoints [randomSP].transform.position;
+			GameObject egg = eggs[randomNumber];
+			Instantiate (egg, spPos, Quaternion.identity);
+			instantiateTimer = 2f;
+		}
+
+	}
+
+	void instantiateLifes() {
+		for (int i = 0; i < lifesEggs.Length; i++) {
+			lifesEggs [i] = Instantiate((GameObject)Resources.Load ("Prefabs/eggLife"),Vector2.zero, Quaternion.identity) as GameObject;
+			lifesEggs [i].transform.SetParent (GameObject.FindGameObjectWithTag ("Canvas").transform, false);
+			lifesEggs [i].transform.position = new Vector2 (100f + ((float)i*30), 460f);
+			if(i < 3)
+				lifesEggs [i].GetComponent<Image> ().enabled = true;
+			else
+				lifesEggs [i].GetComponent<Image> ().enabled = false;
+		}
+	}
+
+	public void drawLifes() {
+		for (int i = 0; i < lifesEggs.Length; i++) {
+			lifesEggs[i].transform.SetParent (GameObject.FindGameObjectWithTag ("Canvas").transform, false);
+			lifesEggs[i].transform.position = new Vector2 (100f + ((float)i*30), 460f);
+		}
+	}
+
+	public void updateLifes(){
+//		Debug.Log ("Update "+currentLifes);
+		for(int i = 0; i < lifesEggs.Length; i++){
+			if(i < currentLifes)
+				lifesEggs [i].GetComponent<Image> ().enabled = true;
+			else
+				lifesEggs [i].GetComponent<Image> ().enabled = false;
+		}
+		drawLifes ();
+	}
+}
